de5d7996fba8cbb1ecb9ccfa1d1a943602828e34
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-17 13:45:20 +0200

    feat: Pass entered Data to the "AllPlaces" Screen

--- models/place.js
+++ models/place.js
@@ -1,9 +1,9 @@
-class Place {
-  constructor(title, imageUri, address, location) {
+export class Place {
+  constructor(title, imageUri, location) {
     this.title = title;
     this.imageUri = imageUri;
-    this.address = address;
-    this.location = location; // * NOTE: { lat: 0.12255, lng: 151.20732 }
+    this.address = location.address;
+    this.location = { lat: location.lat, lng: location.lng }; // * NOTE: { lat: 0.12255, lng: 151.20732 }
     this.id = new Date().toString() + Math.random().toString();
   }
 }
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,8 +1,15 @@
 import PlaceForm from "../components/Places/PlaceForm";
 
-function AddPlace() {
+function AddPlace({ navigation }) {
+  function createPlaceHandler(place) {
+    // * TODO: Insert place into DB on the phone
+    navigation.navigate("AllPlaces", {
+      place: place
+    });
+  }
+
   return (
-    <PlaceForm />
+    <PlaceForm onCreatePlace={createPlaceHandler} />
   )
 }
 
