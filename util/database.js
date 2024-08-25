0994bbba992bee6f83fb27854e5931cd20517c1c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-25 10:42:46 +0200

    feat: Prepare code to Insert Data into SQLite DB

--- util/database.js
+++ util/database.js
@@ -2,9 +2,10 @@ import * as SQLite from 'expo-sqlite';
 
 const database = SQLite.openDatabaseSync('places.db');
 
+// ______________________________________________________________________
 export function init() {
-  return database.runAsync(
-    `CREATE TABLE IF NOT EXISTS places (
+    return database.runAsync(
+        `CREATE TABLE IF NOT EXISTS places (
         id INTEGER PRIMARY KEY NOT NULL, 
         title TEXT NOT NULL,
         imageUri TEXT NOT NULL, 
@@ -13,4 +14,24 @@ export function init() {
         lng REAL NOT NULL
       )
   `);
+}
+
+// ______________________________________________________________________
+export function insertPlace(place) {
+    const promise = new Promise((resolve, reject) => {
+        const { title, imageUri, address, lat, lng } = place;
+        database.runAsync(
+            `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+            [title, imageUri, address, lat, lng],
+            function (err) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(this.lastID);
+            }
+        );
+    });
+
+    return promise;
 }
